a5d900ae69b14a2f2ce532addaa30439e39aedbd
GabrielYamaya <yamaya.gabriel@gmail.com>
2024-03-24 16:34:25 -0300

    First commit

---
+++ src/Calculadora.java
@@ -0,0 +1,26 @@
+//Classe calculadora que possuí as operações matématicas de uma calculadora.		
+public class Calculadora {
+		//Função de soma de dois números inteiros, que retorna o resultado.
+		public int somar(int a, int b) {
+			return a + b;
+		}
+		
+		//Função de subtração de dois números inteiros, que retorna o resultado.
+		public int subtrair(int a, int b) {
+			return a - b;
+		}
+		
+		//Função de multiplicação de dois números inteiros, que retorna o resultado.
+		public int multiplicar(int a, int b) {
+			return a * b;
+		}
+		
+		//Função de divisão de dois números inteiros, que retorna o resultado.
+		public float dividir(int a, int b) {
+			float x, y;
+			x = a;
+			y = b;
+			return x / y;
+		}
+
+}
